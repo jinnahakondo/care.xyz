af6d317cd78ee1d244c5f69f6e0990e8fbf49caf
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-03-05 15:20:15 +0600

    daisyui setup , font setup & favicon

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <button className='text-secondary-content btn btn-secondary'>button</button>
+    </div>
+  )
+}
