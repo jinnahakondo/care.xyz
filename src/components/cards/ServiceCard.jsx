0bf2375931bc283434d4b4e82098d3675387a9a9
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-03-06 11:49:56 +0600

    some update

--- src/components/cards/ServiceCard.jsx
+++ src/components/cards/ServiceCard.jsx
@@ -1,24 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function ServiceCard({ image, title, description }) {
   return (
-    <div className="bg-base-200 p-4 rounded-2xl">
-      <Image
-        src={image}
-        alt={title}
-        width={200}
-        height={200}
-        className="h-49.5 rounded-2xl object-cover w-full"
-      />
-      <div className="mt-4">
+    <div className="bg-base-200 p-4 rounded-2xl h-full flex flex-col group ">
+      {/* card image  */}
+      <div className="h-48 w-full overflow-hidden rounded-2xl">
+        <Image
+          src={image}
+          alt={title}
+          width={200}
+          height={200}
+          className="h-full w-full object-cover  transition duration-300 group-hover:scale-110 "
+        />
+      </div>
+      <div className="mt-4 flex flex-col flex-1">
+        {/* card title  */}
         <h4 className="text-xl font-bold text-neutral mb-1">{title}</h4>
-        <p className="text-xs text-base-content mb-4">
-          {description.slice(0, 80)}
+        {/* card description  */}
+        <p className="text-xs text-base-content mb-4 line-clamp-2">
+          {description}
         </p>
-        <Link href={"/"} className="text-primary text-xs font-medium">
-          Learn more
+        {/* link  */}
+        <Link
+          href={"/"}
+          className="text-primary text-xs font-medium flex items-center gap-2 mt-auto hover:gap-5 transition-[gap]"
+        >
+          Learn more <FaArrowRightLong />
         </Link>
       </div>
     </div>
